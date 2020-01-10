b19ee59b0f55305d81774156b61c5e54ff9f16e5
Piotrek <StiepanR923@gmail.com>
2020-01-10 11:26:25 +0100

    Update Grafika_v1.py

--- Grafika_v1.py
+++ Grafika_v1.py
@@ -3,15 +3,33 @@ from functools import partial
 
 def Bresenham(x1, y1, x2, y2):
     tab = []
-    m_new = 2 * (y2 - y1)
-    slope_error_new = m_new - (x2 - x1)
-    y = y1
-    for x in range(x1, x2 + 1):
-        tab.append((x, y))
-        slope_error_new = slope_error_new + m_new
-        if (slope_error_new >= 0):
-            y = y + 1
-            slope_error_new = slope_error_new - 2 * (x2 - x1)
+    dx = abs(x2 - x1)
+    dy = abs(y2 - y1)
+    x, y = x1, y1
+    sx = -1 if x1 > x2 else 1
+    sy = -1 if y1 > y2 else 1
+
+    if dx > dy:
+        err = dx / 2.0
+        while x != x2:
+            tab.append((x, y))
+            err -= dy
+            if err < 0:
+                y += sy
+                err += dx
+            x += sx
+    else:
+        err = dy / 2.0
+        while y != y2:
+            tab.append((x, y))
+            err -= dx
+            if err < 0:
+                x += sx
+                err += dy
+            y += sy
+
+    tab.append((x2, y2))
+
     return tab
 
 
